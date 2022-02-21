e8ed14c6f640c37b7fdebd5f68a7fd31cf4989cc
peterdactylus <77297874+peterdactylus@users.noreply.github.com>
2022-02-21 19:02:59 +0100

    Update titanic-svm-r.r

--- titanic-svm-r.r
+++ titanic-svm-r.r
@@ -1,17 +1,14 @@
-# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-19T17:27:29.768489Z","iopub.execute_input":"2022-02-19T17:27:29.770980Z","iopub.status.idle":"2022-02-19T17:27:34.265253Z"}}
 library(tidyverse)
 library(data.table)
 library(tidymodels)
 library(doParallel)
 library(themis)
 
-# %% [code] {"execution":{"iopub.status.busy":"2022-02-19T17:27:39.337478Z","iopub.execute_input":"2022-02-19T17:27:39.376729Z","iopub.status.idle":"2022-02-19T17:27:39.769357Z"}}
 train <- read_csv("../input/titanic/train.csv")
 test <- read_csv("../input/titanic/test.csv")
 train <- as.data.table(train)
 test <- as.data.table(test)
 
-# %% [code] {"execution":{"iopub.status.busy":"2022-02-19T17:27:43.310132Z","iopub.execute_input":"2022-02-19T17:27:43.311892Z","iopub.status.idle":"2022-02-19T17:27:43.381725Z"}}
 train[grep("Mr.", train$Name), Title := "Mr."]
 train[grep("Mrs.", train$Name), Title := "Mrs."]
 train[grep("Miss.", train$Name), Title := "Miss."]
@@ -27,14 +24,12 @@ train[, Survived := as.character(Survived)]
 
 head(train)
 
-# %% [code] {"execution":{"iopub.status.busy":"2022-02-19T17:33:05.717631Z","iopub.execute_input":"2022-02-19T17:33:05.719471Z","iopub.status.idle":"2022-02-19T17:33:05.782303Z"}}
 train_set <- train[, .(Survived, Pclass, Sex, Age, SibSp, Parch, Fare, Embarked, Title)]
 test_set <- test[, .(Pclass, Sex, Age, SibSp, Parch, Fare, Embarked, Title)]
 
 head(train_set)
 head(test_set)
 
-# %% [code] {"execution":{"iopub.status.busy":"2022-02-19T17:37:27.435929Z","iopub.execute_input":"2022-02-19T17:37:27.437839Z","iopub.status.idle":"2022-02-19T17:37:27.927019Z"}}
 svm_spec <- svm_rbf(
     cost = tune(), 
     rbf_sigma = tune()
@@ -62,7 +57,6 @@ svm_grid <- grid_latin_hypercube(
     size = 15
 )
 
-# %% [code] {"execution":{"iopub.status.busy":"2022-02-19T17:37:44.807447Z","iopub.execute_input":"2022-02-19T17:37:44.809095Z","iopub.status.idle":"2022-02-19T17:39:07.596348Z"}}
 c <- detectCores()
 registerDoParallel(cores = c)
 rm(c)
@@ -79,15 +73,12 @@ print(proc.time() - ptm)
   
 stopImplicitCluster()
 
-# %% [code] {"execution":{"iopub.status.busy":"2022-02-19T17:39:22.782735Z","iopub.execute_input":"2022-02-19T17:39:22.784846Z","iopub.status.idle":"2022-02-19T17:39:22.862284Z"}}
 show_best(svm_res, metric = "accuracy", n = 5)
 
-# %% [code]
 svm_final <- finalize_model(svm_spec, select_best(svm_res, metric = "accuracy"))
 svm_model <- fit(svm_final, Survived ~ ., juice(cust_rec))
 pred <- predict(svm_model, test_baked)
 
-# %% [code]
 sub <- as.data.table(cbind(test[, .(PassengerId)], pred))
 setnames(sub, ".pred_class", "Survived")
 write_csv(sub, "submission.csv")
