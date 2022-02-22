804145fc036981d4eb486769efaf8d2b46c4fb71
peterdactylus <77297874+peterdactylus@users.noreply.github.com>
2022-02-22 18:58:23 +0100

    Update mnist-cnn-torch-r.r

--- mnist-cnn-torch-r.r
+++ mnist-cnn-torch-r.r
@@ -1,18 +1,14 @@
-# %% [code] {"_execution_state":"idle","jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-22T16:25:39.565425Z","iopub.execute_input":"2022-02-22T16:25:39.567041Z","iopub.status.idle":"2022-02-22T16:25:39.582102Z"}}
 library(tidyverse)
 library(data.table)
 library(rsample)
 library(torch)
 
-# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-22T16:25:39.584266Z","iopub.execute_input":"2022-02-22T16:25:39.585519Z","iopub.status.idle":"2022-02-22T16:25:41.624096Z"}}
 train <- read_csv("train.csv/train.csv")
 test <- read_csv("test.csv/test.csv")
 
-# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-22T16:25:41.626307Z","iopub.execute_input":"2022-02-22T16:25:41.627571Z","iopub.status.idle":"2022-02-22T16:25:44.776829Z"}}
 split <- initial_split(train, 0.8)
 split
 
-# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-22T16:25:44.779109Z","iopub.execute_input":"2022-02-22T16:25:44.780322Z","iopub.status.idle":"2022-02-22T16:25:44.960385Z"}}
 par(mfcol=c(3, 6))
 par(mar=c(0, 0, 3, 0), xaxs='i', yaxs='i')
 for(i in 1:18) {
@@ -21,7 +17,6 @@ for(i in 1:18) {
   image(1:28, 1:28, tmp, col = gray((0:255)/255), main = paste(train$label[i]), xaxt = "n")
 }
 
-# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-22T16:25:44.964192Z","iopub.execute_input":"2022-02-22T16:25:44.965496Z","iopub.status.idle":"2022-02-22T16:25:44.977988Z"}}
 mnist_dataset <- dataset(
   
   name = "mnist_dataset",
@@ -50,16 +45,13 @@ mnist_dataset <- dataset(
   }  
 )
 
-# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-22T16:25:44.980241Z","iopub.execute_input":"2022-02-22T16:25:44.981534Z","iopub.status.idle":"2022-02-22T16:25:45.354871Z"}}
 train_set <- mnist_dataset(training(split))
 val_set <- mnist_dataset(testing(split))
 test_set <- mnist_dataset(test, label = FALSE)
 
-# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-22T16:25:45.357491Z","iopub.execute_input":"2022-02-22T16:25:45.358818Z","iopub.status.idle":"2022-02-22T16:25:45.382783Z"}}
 train_set[1][[1]]$size()
 train_set$.getitem(5)
 
-# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-22T16:25:45.384938Z","iopub.execute_input":"2022-02-22T16:25:45.386115Z","iopub.status.idle":"2022-02-22T16:25:45.600200Z"}}
 train_dl <- train_set %>% dataloader(batch_size = 128, shuffle = TRUE)
 val_dl <- val_set %>% dataloader(batch_size = 128)
 test_dl <- test_set %>% dataloader(batch_size = 128)
@@ -67,25 +59,19 @@ train_iter <- train_dl$.iter()
 train_iter$.next()
 rm(train_iter)
 
-# %% [code] {"execution":{"iopub.status.busy":"2022-02-22T16:25:45.602298Z","iopub.execute_input":"2022-02-22T16:25:45.603535Z","iopub.status.idle":"2022-02-22T16:25:45.621248Z"},"jupyter":{"outputs_hidden":false}}
 val_loss <- function(model, dl) {
   model$eval()
   l <- c()
   coro::loop(for (b in dl) {
-    # get model predictions
     output <- model(b[[1]])
-    # get label
     label <- b[[2]]
-    # calculate loss
     loss <- nnf_cross_entropy(output, label[,1])
-    # track losses
     l <- c(l, loss$item())
   })
   model$train()
   return(l)
 }
 
-# %% [code] {"execution":{"iopub.status.busy":"2022-02-22T16:25:45.623684Z","iopub.execute_input":"2022-02-22T16:25:45.624866Z","iopub.status.idle":"2022-02-22T16:25:45.634607Z"},"jupyter":{"outputs_hidden":false}}
 pred <- function(model, dl) {
   model$eval()
   preds <- c()
@@ -101,7 +87,6 @@ pred <- function(model, dl) {
   return(preds)
 }
 
-# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-22T16:25:45.636672Z","iopub.execute_input":"2022-02-22T16:25:45.637827Z","iopub.status.idle":"2022-02-22T16:25:45.648007Z"}}
 net <- nn_module(
   "MNIST Net", 
   initialize = function() {
@@ -134,27 +119,18 @@ net <- nn_module(
   }
 )
 
-# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-22T16:25:45.650041Z","iopub.execute_input":"2022-02-22T16:25:45.651242Z","iopub.status.idle":"2022-02-22T16:25:45.690205Z"}}
 model = net()
 optimizer = optim_adam(model$parameters)
 
-# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2022-02-22T16:25:45.692321Z","iopub.execute_input":"2022-02-22T16:25:45.693537Z","iopub.status.idle":"2022-02-22T16:26:02.772278Z"}}
 for (epoch in 1:3) {
   i <- 1
   coro::loop(for (b in train_dl) {
-    # make sure each batch's gradient updates are calculated from a fresh start
     optimizer$zero_grad()
-    # get model predictions
     output <- model(b[[1]])
-    # get label
     label <- b[[2]]
-    # calculate loss
     loss <- nnf_cross_entropy(output, label[,1])
-    # calculate gradient
     loss$backward()
-    # apply weight updates
     optimizer$step()
-    # track losses
     i <- i + 1
     if(i %% 50 == 0){
       cat(paste("Epoch:", epoch, "| Batch:", i, "| Loss:", loss$item(), "\n"))
@@ -164,7 +140,6 @@ for (epoch in 1:3) {
   cat(paste("Epoch:", epoch, "| Val Loss:", mean(vl), "\n"))
 }
 
-# %% [code]
 preds <- pred(model, test_dl)
 preds <- as.data.table(preds)
 preds[, ImageId := seq(1, nrow(preds))]
